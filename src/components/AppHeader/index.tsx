8db9e69d0c2c7f2113206f1c800c528147025229
summerSmithOn <summer.smith@crescent.foundation>
2022-07-01 02:42:47 +0900

    feat: app chain toggling feature into AppChainListBox, the new component, from AppSettingWidget

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,3 +1,4 @@
+import AppChainListBox from 'components/AppChainListBox'
 import AppSettingWidget from 'components/AppSettingWidget'
 import Logo from 'components/Logo'
 import NavigationTab from 'components/NavigationTab'
@@ -8,15 +9,18 @@ export default function Header() {
     <header
       className={`relative flex items-center justify-between w-full p-4 bg-lightCRE border-b-grayCRE-100 border-b md:shadow-glow-wide-l`}
     >
-      <div className="flex justify-start items-center space-x-6">
+      <div className="grow-0 shrink-0 flex justify-start items-center space-x-6">
         <div className="flex justify-start items-center space-x-2">
           <Logo className="h-10 py-2" src={CRESCENT_LOGO_IMG_URL} />
-          <h1 className="inline-flex justify-start items-center TYPO-H1">Admin</h1>
+          <h1 className="hidden inline-flex justify-start items-center TYPO-H2 md:block md:TYPO-H1">Admin</h1>
         </div>
         <NavigationTab />
       </div>
 
-      <AppSettingWidget />
+      <div className="grow shrink flex justify-end items-center space-x-1">
+        <AppChainListBox />
+        <AppSettingWidget />
+      </div>
     </header>
   )
 }
