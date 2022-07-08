8bc83cc187ae8834a5d9f6873f0ee8717ed3813c
summerSmithOn <summer.smith@crescent.foundation>
2022-07-08 18:47:39 +0900

    wip: Farm staked amount comparison hooks are wip

--- src/data/useLCD.ts
+++ src/data/useLCD.ts
@@ -1,5 +1,5 @@
 import useAppSWR, { lcdReturnGenerator } from 'data/useAppSWR'
-import type { BalanceLCD } from 'types/account'
+import type { BalanceLCD, FarmRewardsLCDRaw } from 'types/account'
 import type { LCDResponseViaSWR } from 'types/api'
 import type { BlockLCD } from 'types/block'
 
@@ -12,6 +12,30 @@ export function useAllBalanceLCD({ address, fetch = true }: { address: string; f
   })
   return lcdReturnGenerator({ data, error })
 }
+
+export function useAllFarmRewardsLCD({ address, fetch = true }: { address: string; fetch?: boolean }, interval = 0) {
+  const { data, error }: LCDResponseViaSWR<FarmRewardsLCDRaw> = useAppSWR(
+    `/crescent/farming/v1beta1/rewards/${address}`,
+    {
+      interval,
+      type: 'rpc-rest',
+      fetch,
+    }
+  )
+  return lcdReturnGenerator({ data, error })
+}
+
+// export function useAllFarmStakedLCD({ address, fetch = true }: { address: string; fetch?: boolean }, interval = 0) {
+//   const { data, error }: LCDResponseViaSWR<FarmRewardsLCDRaw> = useAppSWR(
+//     `/crescent/farming/v1beta1/stakings/${address}`,
+//     {
+//       interval,
+//       type: 'rpc-rest',
+//       fetch,
+//     }
+//   )
+//   return lcdReturnGenerator({ data, error })
+// }
 
 export function useLatestBlockLCD({ fetch = true }: { fetch?: boolean }, interval = 0) {
   const { data, error }: LCDResponseViaSWR<BlockLCD> = useAppSWR(`/blocks/latest`, {
