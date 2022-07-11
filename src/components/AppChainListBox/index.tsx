e90c6df7cf1c7cd6af11e1dcbb1b9956d8348a4a
summerSmithOn <summer.smith@crescent.foundation>
2022-07-11 15:20:09 +0900

    feat: app chain toggling feature into AppChainListBox, the new component, from AppSettingWidget

---
+++ src/components/AppChainListBox/index.tsx
@@ -0,0 +1,91 @@
+import { Listbox } from '@headlessui/react'
+import Icon from 'components/Icon'
+import { CHAIN_IDS } from 'constants/chain'
+import { useAtom } from 'jotai'
+import { useEffect, useState } from 'react'
+import { ChainId, chainIdAtomRef } from 'state/atoms'
+
+interface ChainOption {
+  chainId: ChainId
+  label: string
+}
+
+const CHAINS: ChainOption[] = [
+  { chainId: CHAIN_IDS.MAINNET, label: 'Mainnet' },
+  { chainId: CHAIN_IDS.MOONCAT, label: 'Testnet' },
+]
+
+export default function AppChainListBox() {
+  // chain state
+  const [chainIdAtom, setChainIdAtom] = useAtom(chainIdAtomRef)
+
+  const [chainIndex, setChainIndex] = useState<number>(0)
+
+  const onChange = (index: number) => {
+    const chainId = CHAINS[index].chainId
+    setChainIdAtom({ chainId })
+  }
+
+  const colorBodyByChain = (chainId: ChainId) => {
+    if (chainId === CHAIN_IDS.MOONCAT) {
+      document.body.classList.add('testnet')
+    } else {
+      document.body.classList.remove('testnet')
+    }
+  }
+
+  useEffect(() => {
+    colorBodyByChain(chainIdAtom)
+    setChainIndex(CHAINS.findIndex((item) => item.chainId === chainIdAtom))
+  }, [chainIdAtom])
+
+  return (
+    <div className="relative w-full max-w-[160px]">
+      <Listbox value={chainIndex} onChange={onChange}>
+        <Listbox.Button
+          className={({ open }) =>
+            `w-full rounded-lg bg-grayCRE-300 text-left text-white px-2 py-2 outline-0 transition-all md:px-4 hover:bg-grayCRE-200 ${
+              open ? '!bg-grayCRE-200 rounded-b-none' : ''
+            }`
+          }
+        >
+          {({ open }) => {
+            return (
+              <div className="flex justify-between items-center space-x-4">
+                <div className="grow shrink">
+                  <div className="flex items-center overflow-hidden TYPO-BODY-XS !font-bold whitespace-nowrap md:TYPO-BODY-S">
+                    {CHAINS[chainIndex].label}
+                  </div>
+                </div>
+                <div className="grow-0 shrink-0">
+                  <Icon type={open ? `expandless` : `expandmore`} className="w-6 h-6" />
+                </div>
+              </div>
+            )
+          }}
+        </Listbox.Button>
+
+        <Listbox.Options
+          className={`absolute w-full rounded-lg rounded-t-none bg-white border-2 border-t-0 border-grayCRE-200 max-h-48 overflow-y-auto outline-0 transition-all`}
+        >
+          {CHAINS.map((chain: ChainOption, index) => (
+            <Listbox.Option
+              key={index}
+              value={index}
+              className={({ active }) =>
+                `TYPO-BODY-XS text-grayCRE-500 cursor-pointer px-2 py-2 outline-0 md:TYPO-BODY-S md:px-4 ${
+                  active ? 'bg-grayCRE-100' : ''
+                }`
+              }
+            >
+              <div className="flex">
+                {/* <CoinLogo logoUrl={asset?.logoUrl} ticker={asset?.ticker} css={'w-5 h-5 mr-1'} /> */}
+                <div> {chain.label}</div>
+              </div>
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </div>
+  )
+}
