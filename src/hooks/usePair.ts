f4a287ab329b2e72d6bf4ec900fd5e20d3a1526a
summerSmithOn <summer.smith@crescent.foundation>
2022-07-11 15:20:06 +0900

    feat: pool token price added to Asset table list data

--- src/hooks/usePair.ts
+++ src/hooks/usePair.ts
@@ -3,6 +3,7 @@ import useAsset from 'hooks/useAsset'
 import { useAtom } from 'jotai'
 import { useCallback, useMemo } from 'react'
 import { allPairInfoAtomRef, allPairLiveAtomRef } from 'state/atoms'
+import type { PairInfo, PairLive } from 'types/pair'
 
 const usePair = () => {
   const [allPairInfoAtom] = useAtom(allPairInfoAtomRef)
@@ -26,9 +27,48 @@ const usePair = () => {
   //   [allPairLiveAtom, allAsset]
   // )
 
+  const allPairLive = useMemo(() => {
+    return allPairLiveAtom.map((pair) => {
+      const baseDenomExponent = allAsset.find((asset) => asset.denom === pair.baseDenom)?.exponent ?? 0
+      return {
+        ...pair,
+        lastPrice: new BigNumber(pair.lastPrice),
+        predPrice: new BigNumber(pair.predPrice),
+        vol_24: new BigNumber(pair.vol_24).dividedBy(10 ** baseDenomExponent),
+        totalReserved: pair.totalReserved.map((item) => {
+          const exponent = allAsset.find((asset) => asset.denom === item.denom)?.exponent ?? 0
+          return {
+            ...item,
+            priceOracle: new BigNumber(item.priceOracle),
+            amount: new BigNumber(item.amount).dividedBy(10 ** exponent),
+          }
+        }),
+      }
+    }) as PairLive[]
+  }, [allPairLiveAtom, allAsset])
+
+  const allPairInfo = useMemo(() => {
+    return allPairInfoAtom.map((pair) => {
+      return {
+        ...pair,
+        lastPrice: new BigNumber(pair.lastPrice),
+        pools: pair.pools.map((item) => {
+          return {
+            ...item,
+            totalSupply: new BigNumber(item.totalSupply),
+            reserved: item.reserved.map((reserve) => {
+              const exponent = allAsset.find((asset) => asset.denom === reserve.denom)?.exponent ?? 0
+              return { ...reserve, amount: new BigNumber(reserve.amount).dividedBy(10 ** exponent) }
+            }),
+          }
+        }),
+      }
+    }) as PairInfo[]
+  }, [allPairInfoAtom, allAsset])
+
   const getTVLUSDbyDenom = useCallback(
     (denom: string) => {
-      const tvl = allPairLiveAtom.reduce((tvl, pair) => {
+      const tvl = allPairLive.reduce((tvl, pair) => {
         const reserveByPair = pair.totalReserved
           .filter((item) => item.denom === denom)
           .reduce((accm, item) => accm.plus(new BigNumber(item.amount)), new BigNumber(0))
@@ -36,41 +76,60 @@ const usePair = () => {
       }, new BigNumber(0))
 
       const baseAsset = allAsset.find((item) => item.denom === denom)
-      const exponent = baseAsset?.exponent ?? 0
+      // const exponent = baseAsset?.exponent ?? 0
       const priceOracle = baseAsset?.live?.priceOracle ?? 0
-      return tvl.dividedBy(10 ** exponent).multipliedBy(priceOracle)
+      return tvl.multipliedBy(priceOracle)
     },
-    [allPairLiveAtom, allAsset]
+    [allPairLive, allAsset]
   )
 
   const tvlUSD = useMemo(() => {
-    return allPairLiveAtom.reduce((tvl, pair) => {
+    return allPairLive.reduce((tvl, pair) => {
       const reserveByPair = pair.totalReserved.reduce((accm, reserve) => {
         const baseAsset = allAsset.find((item) => item.denom === reserve.denom)
-        const exponent = baseAsset?.exponent ?? 0
+        // const exponent = baseAsset?.exponent ?? 0
         const priceOracle = baseAsset?.live?.priceOracle ?? 0
-        const reserveUSD = new BigNumber(reserve.amount).dividedBy(10 ** exponent).multipliedBy(priceOracle)
+        const reserveUSD = new BigNumber(reserve.amount).multipliedBy(priceOracle)
         return accm.plus(reserveUSD)
       }, new BigNumber(0))
       return tvl.plus(reserveByPair)
     }, new BigNumber(0))
-  }, [allPairLiveAtom, allAsset])
+  }, [allPairLive, allAsset])
 
   const getVol24USDbyDenom = useCallback(
-    (denom: string, exponent: number) => {
-      return allPairLiveAtom
+    (denom: string) => {
+      return allPairLive
         .filter((pair) => pair.baseDenom === denom || pair.quoteDenom === denom)
         .reduce((accm, pair) => {
-          const vol24 = new BigNumber(pair.vol_24).dividedBy(10 ** exponent) // in baseDenom
+          const vol24 = pair.vol_24 // vol_24 in baseDenom
           const baseAsset = pair.totalReserved.find((item) => item.denom === pair.baseDenom)
-          const vol24USD = vol24.multipliedBy(new BigNumber(baseAsset ? baseAsset.priceOracle : 0))
+          const vol24USD = vol24.multipliedBy(baseAsset?.priceOracle ?? 0)
           return accm.plus(vol24USD)
         }, new BigNumber(0))
     },
-    [allPairLiveAtom]
+    [allPairLive]
   )
 
-  return { allPairInfoAtom, allPairLiveAtom, tvlUSD, getTVLUSDbyDenom, getVol24USDbyDenom }
+  // const getPoolTokenPriceOracle = useCallback(
+  //   (denom: string) => {
+  //     const pools = allPairInfo.reduce((accm: PoolInPair[], pair) => accm.concat(pair.pools), [])
+  //     const pool = pools.find((pool) => pool.poolDenom === denom)
+
+  //     if (!pool) return new BigNumber(0)
+
+  //     const poolReservedUSD = pool.reserved.reduce((accm, reserve) => {
+  //       const priceOracle =
+  //         allAsset.find((asset) => asset.denom === reserve.denom)?.live?.priceOracle ?? new BigNumber(0)
+  //       const reserveUSD = reserve.amount.multipliedBy(priceOracle)
+  //       return accm.plus(reserveUSD)
+  //     }, new BigNumber(0))
+
+  //     return poolReservedUSD.dividedBy(pool.totalSupply)
+  //   },
+  //   [allPairInfo, allAsset]
+  // )
+
+  return { allPairLive, allPairInfo, tvlUSD, getTVLUSDbyDenom, getVol24USDbyDenom }
 }
 
 export default usePair
--- src/utils/amount.ts
+++ src/utils/amount.ts
@@ -23,3 +23,24 @@ export const formatUSDAmount = (amt: BigNumber | undefined, digits = 2, average 
     currencySymbol: '≈',
   })
 }
+
+// tmp to parse scientific notation for log digits number in js
+export const longDigitsToFixed = (x: number) => {
+  let fixed: string | undefined
+
+  if (Math.abs(x) < 1.0) {
+    const e = parseInt(x.toString().split('e-')[1])
+    if (e) {
+      x *= Math.pow(10, e - 1)
+      fixed = '0.' + new Array(e).join('0') + x.toString().substring(2)
+    }
+  } else {
+    let e = parseInt(x.toString().split('+')[1])
+    if (e > 20) {
+      e -= 20
+      x /= Math.pow(10, e)
+      fixed += new Array(e + 1).join('0')
+    }
+  }
+  return fixed ?? `${x}`
+}
