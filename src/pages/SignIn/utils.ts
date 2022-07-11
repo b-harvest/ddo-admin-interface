872736619cfc1314272efcc515a71720038224a4
summerSmithOn <summer.smith@crescent.foundation>
2022-07-11 15:20:19 +0900

    chore: logs commented & deploy script added

--- src/pages/SignIn/utils.ts
+++ src/pages/SignIn/utils.ts
@@ -7,14 +7,14 @@ export const isValidUser = (profileObj: GoogleUserProfile, validHostName: string
 }
 
 export const refreshTokenSetup = async (res) => {
-  console.log(res.tokenObj)
+  // console.log(res.tokenObj)
   // Timing to renew access token
   let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
 
   const refreshToken = async () => {
     const newAuthRes = await res.reloadAuthResponse()
     refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
-    console.log('newAuthRes:', newAuthRes)
+    //console.log('newAuthRes:', newAuthRes)
     // saveUserToken(newAuthRes.access_token);  <-- save new token
     localStorage.setItem('authToken', newAuthRes.id_token)
 
