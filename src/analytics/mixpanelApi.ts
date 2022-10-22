8146dc09724c13e747f4bda55796536ec072e20a
summerSmithOn <summer.smith@crescent.foundation>
2022-10-22 23:39:50 +0900

    fix: mixpanel ingestion api cors error handled

--- src/analytics/mixpanelApi.ts
+++ src/analytics/mixpanelApi.ts
@@ -3,10 +3,9 @@ import type { GoogleUserProfile } from 'types/user'
 
 const api = axios.create({
   baseURL: 'https://api.mixpanel.com',
-  headers: {
-    Accept: 'text/plain',
-  },
 })
+
+delete api.defaults.headers.common['Content-Type']
 
 export const setProfile = (body: [{ $token: string; $distinct_id: string; $set: GoogleUserProfile }]) =>
   api.post('/engage#profile-set', body)
