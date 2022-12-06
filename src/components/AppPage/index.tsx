3f790125c36c34a57485fb578c72417e2a6f090e
summerSmithOn <summer.smith@crescent.foundation>
2022-12-06 13:24:25 +0900

    style: max width for all pages on wide-width device for users to feel easy to read all

--- src/components/AppPage/index.tsx
+++ src/components/AppPage/index.tsx
@@ -1,15 +1,19 @@
 import usePages from 'pages/hooks/usePages'
 import { ReactNode } from 'react'
 
-export default function AppPage({ children, className }: { children: ReactNode; className?: string }) {
+export default function AppPage({ children, className = '' }: { children: ReactNode; className?: string }) {
   const { page } = usePages()
 
   return (
-    <div className={`${className} relative px-4 pt-[2rem] pb-[calc(2.5rem+8px+4rem)] overflow-hidden md:px-12`}>
-      <h2 className="block TYPO-H2 text-black text-left mb-8 dark:text-white" style={{ wordBreak: 'keep-all' }}>
-        {page?.showPageName && page?.pageName}
-      </h2>
-      {children}
+    <div
+      className={`${className} relative flex justify-center px-4 pt-[2rem] pb-[calc(2.5rem+8px+4rem)] overflow-hidden md:px-12`}
+    >
+      <div className="w-full max-w-[90rem]">
+        <h2 className="block TYPO-H2 text-black text-left mb-8 dark:text-white" style={{ wordBreak: 'keep-all' }}>
+          {page?.showPageName && page?.pageName}
+        </h2>
+        {children}
+      </div>
     </div>
   )
 }
